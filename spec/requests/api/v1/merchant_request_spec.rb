9058560715a1fed939e95fa93907f894d97df7f3
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-27 15:20:39 -0700

    Creates show endpoint for merchants.

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -13,4 +13,15 @@ describe "Merchants API" do
     expect(merchants.count).to eq(3)
 
   end
+
+  it 'can get one merchant by id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
 end
