b912a4e3edd64ef2900a4b92a22d1dfd504e747a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-27 21:03:03 -0700

    Model and relationship testing for invoice and invoice item.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of :status}
+  end
+  describe 'relationships' do
+    it {should belong_to :merchant}
+    it {should belong_to :customer}
+    it {should have_many :invoice_items}
+    it {should have_many(:items).through(:invoice_items)}
+  end
 end
