306cff0e9316c7eb2085745b35de884d0a39bcfc
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-29 09:48:41 -0700

    Adds formatting for items unit price.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :unit_price
+  attributes :name, :description, :merchant_id
+
+  attribute :unit_price do |object|
+    "%.2f" % (object.unit_price.to_f / 100).to_s
+  end
+
+
 end
