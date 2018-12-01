9405a7e6bbb2f36fecbad11c2abd8a1ddca7fb58
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-30 23:12:02 -0700

    Search controller for merchants.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
    namespace :v1 do
+     namespace :merchants do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+      end
      resources :merchants, only: [:index, :show]
      resources :items, only: [:index, :show]
      resources :transactions, only: [:index, :show]
