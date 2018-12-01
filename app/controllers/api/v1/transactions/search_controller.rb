1aa766cb39f617f835a7df7599dcc1f24cace6e5
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-30 23:29:11 -0700

    transaction search controller.

---
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::Transactions::SearchController < ApplicationController
+
+  def index
+    render json:  TransactionSerializer.new(Transaction.where(search_params))
+  end
+
+  def show
+    render json: TransactionSerializer.new(Transaction.find_by(search_params))
+  end
+  private
+
+    def search_params
+      params.permit(:id, :invoice_id, :credit_card_number, :result, :created_at, :updated_at)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,12 @@ Rails.application.routes.draw do
 
       namespace :items do
         get 'find_all', to: 'search#index'
-        get 'find', to: 'search#show'  
+        get 'find', to: 'search#show'
+      end
+
+      namespace :transactions do
+        get 'find_all', to: 'search#index'
+        get 'find', to: 'search#show'
       end
      resources :merchants, only: [:index, :show]
      resources :items, only: [:index, :show]
