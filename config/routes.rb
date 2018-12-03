c89ee1b92398603ee8739ad6239ee3fe3107d243
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-02 21:12:14 -0700

    Invoice items controller for items.

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
       namespace :items do
         get 'find_all', to: 'search#index'
         get 'find', to: 'search#show'
+        get '/:id/invoice_items', to: 'invoice_items#index'
       end
 
       namespace :transactions do
