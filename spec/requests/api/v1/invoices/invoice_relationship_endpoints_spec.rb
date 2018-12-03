7b62acd118ce49caec6529b33e2e06cc17a7e43f
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-02 20:50:56 -0700

    Nested items invoice.

--- spec/requests/api/v1/invoices/invoice_relationship_endpoints_spec.rb
+++ spec/requests/api/v1/invoices/invoice_relationship_endpoints_spec.rb
@@ -29,23 +29,23 @@ describe 'Invoice relationships' do
     merchant = JSON.parse(response.body)
     expect(merchant["data"]["id"]).to eq(merchant_id.to_s)
   end
-  # it 'returns all items for an invoice'do
-  #   merchant_id  = create(:merchant).id
-  #   customer_id  = create(:customer).id
-  #   invoice  = create(:invoice, merchant_id: merchant_id, customer_id: customer_id)
-  #   item1 = create(:item, merchant_id: merchant_id)
-  #   item2 = create(:item, merchant_id: merchant_id)
-  #   invoice_item1 = create(:invoice_item, invoice_id: invoice.id, item_id: item1.id)
-  #   invoice_item2 = create(:invoice_item, invoice_id: invoice.id, item_id: item2.id)
-  #
-  #   get "/api/v1/invoices/#{invoice.id}/items"
-  #
-  #   expect(response).to be_successful
-  #
-  #   items = JSON.parse(response.body)
-  #
-  #   expect(items.count).to eq(2)
-  # end
+  it 'returns all items for an invoice'do
+    merchant_id  = create(:merchant).id
+    customer_id  = create(:customer).id
+    invoice  = create(:invoice, merchant_id: merchant_id, customer_id: customer_id)
+    item1 = create(:item, merchant_id: merchant_id)
+    item2 = create(:item, merchant_id: merchant_id)
+    invoice_item1 = create(:invoice_item, invoice_id: invoice.id, item_id: item1.id)
+    invoice_item2 = create(:invoice_item, invoice_id: invoice.id, item_id: item2.id)
+
+    get "/api/v1/invoices/#{invoice.id}/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq(2)
+  end
 
   # it 'returns all invoice items for an invoice'do
   #   merchant_id  = create(:merchant).id
